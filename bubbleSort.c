bf08f101da6a1417d703abcb838ea6a4d33ecf0d
Muhammad Raza <mhraza51@gmail.com>
2026-03-28 19:13:20 -0500

    updated comments

--- bubbleSort.c
+++ bubbleSort.c
@@ -6,6 +6,12 @@ void BubbleSort()
     int my_array[] = {64, 34, 25, 12, 22, 11, 90, 5};
     int n = sizeof(my_array) / sizeof(my_array[0]);
 
+    printf("Original array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", my_array[i]);
+    }
+    printf("\n");
+
     for (int i = 0; i < n-1; i++) {
         for (int j = 0; j < n-i-1; j++) {
             if (my_array[j] > my_array[j+1]) {
